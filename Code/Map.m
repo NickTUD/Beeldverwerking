8caa1bedfab348c9dba75109df2f0e9c6c343552
sevanderheijde <S.E.vanderHeijden@student.tudelft.nl>
2017-01-18 11:21:04 +0100

    Mapping to get the correct character

---
+++ Code/Map.m
@@ -0,0 +1,6 @@
+function [output] = Map(input)
+    keySet = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36};
+    valueSet = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    mapObj = containers.Map(keySet,valueSet,'UniformValues',false);
+    output = mapObj(input);
+end
