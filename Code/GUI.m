3afafd7b8669f095bae20277e478873c0136c86e
Matthijs de Groot <mawdegroot@>
2017-01-19 10:00:43 +0100

    Fixed dashes

--- Code/GUI.m
+++ Code/GUI.m
@@ -151,17 +151,13 @@ if(handles.vidimported && ~handles.running)
         [~, y] = size(array);
         binaryImage = array{1,1};
 
-        string = num2str(getCharacter(binaryImage, handles.characterTable));
-              
-        for j = 2:y
-            for s = 1:2
-                binaryImage = array{1,j};
-                if loc(s) == j
-                    string = strcat(string, num2str(getCharacter(binaryImage, handles.characterTable))+'-');
-                else
-                    string = strcat(string, num2str(getCharacter(binaryImage, handles.characterTable)));
-                end
+        string = '';
+        for j = 1:y
+            binaryImage = array{1,j};
+            if sum((loc == j-1)) > 0
+                string = sprintf('%s-', string);
             end
+            string = sprintf('%s%s', string, getCharacter(binaryImage, handles.characterTable));
         end
         
     %Display the first frame in the table.
@@ -179,17 +175,13 @@ if(handles.vidimported && ~handles.running)
             [array,loc] = plate2letters(ROIs.Image{k});
             [~, y] = size(array);
             binaryImage = array{1,1};
-            string = num2str(getCharacter(binaryImage, handles.characterTable));
-
-            for j = 2:y
-                for t = 1:2
-                    binaryImage = array{1,j};
-                    if loc(t) == j
-                        string = strcat(string, num2str(getCharacter(binaryImage, handles.characterTable))+'-');
-                    else
-                        string = strcat(string, num2str(getCharacter(binaryImage, handles.characterTable)));
-                    end
+            string = '';
+            for j = 1:y
+                binaryImage = array{1,j};
+                if sum((loc == j-1)) > 0
+                    string = sprintf('%s-', string);
                 end
+                string = sprintf('%s%s', string, getCharacter(binaryImage, handles.characterTable));
             end
             
             %The current table with all entries.
